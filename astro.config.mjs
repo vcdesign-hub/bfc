d9fdc8b76eb8fbab3bee2ca5a9978da65cabfe4c
vcdesign-hub <rpasno7@gmail.com>
2026-03-30 23:21:16 +0800

    updated site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,24 +6,23 @@ import node from '@astrojs/node';
 import vercel from '@astrojs/vercel/serverless';
 import sharpImageService from 'astro/assets/services/sharp';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 const isVercel = process.env.VERCEL === '1';
 const site = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:4321';
 
 export default defineConfig({
-	site,
-	output: 'server',
-	adapter: isVercel ? vercel() : node({ mode: 'standalone' }),
-	integrations: [
-		tailwind(),
-		sitemap({
-			filter: (page) => {
-				const { pathname } = new URL(page);
-				return pathname !== '/privacy' && pathname !== '/legal-notice';
-			},
-		}),
-	],
-	image: {
-		service: sharpImageService,
-	},
+    site,
+    output: 'server',
+    adapter: isVercel ? vercel() : node({ mode: 'standalone' }),
+    integrations: [tailwind(), sitemap({
+        filter: (page) => {
+            const { pathname } = new URL(page);
+            return pathname !== '/privacy' && pathname !== '/legal-notice';
+        },
+		}), react()],
+    image: {
+        service: sharpImageService,
+    },
 });
